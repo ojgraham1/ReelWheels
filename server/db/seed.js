602b4f156819a64e5d3006e57b01f21ba66b143a
Curtis Patterson <Dpatter94@yahoo.com>
2024-06-28 11:05:54 -0400

    Current Main

--- server/db/seed.js
+++ server/db/seed.js
@@ -7,11 +7,11 @@ async function main() {
 
   const maya = await prisma.users.create({
     data: {
-      username: "mimmo888",
+      username: "luckystar",
       password: "lucky",
       firstName: "Maya",
       lastName: "Obeidat",
-      email: "mimmo@gmail.com",
+      email: "maya@gmail.com",
       address: "777 Lucky St.",
       phoneNumber: "(770)777-7777",
       birthdate: new Date(2001, 2, 1),
@@ -20,6 +20,56 @@ async function main() {
   });
 
   console.log(maya);
+
+  console.log("seeding database");
+
+  const thomas = await prisma.users.create({
+    data: {
+      username: "princessprisma",
+      password: "princess",
+      firstName: "Thomas",
+      lastName: "Graham",
+      email: "thomas@gmail.com",
+      address: "444 Princess Ln.",
+      phoneNumber: "(404)444-4444",
+      birthdate: new Date(1978, 3, 12),
+      isAdmin: true,
+    },
+  });
+
+  console.log(thomas);
+
+  const olivia = await prisma.users.create({
+    data: {
+      username: "catattack",
+      password: "kitty",
+      firstName: "Olivia",
+      lastName: "Graham",
+      email: "olivia@gmail.com",
+      address: "999 Kitten Ln.",
+      phoneNumber: "(202)999-999",
+      birthdate: new Date(2000, 7, 26),
+      isAdmin: true,
+    },
+  });
+
+  console.log(olivia);
+
+  const daniel = await prisma.users.create({
+    data: {
+      username: "bravesfan1",
+      password: "bball",
+      firstName: "Daniel",
+      lastName: "Patterson",
+      email: "daniel@gmail.com",
+      address: "333 Baseball Cr.",
+      phoneNumber: "(470)333-3333",
+      birthdate: new Date(1886, 5, 23),
+      isAdmin: true,
+    },
+  });
+
+  console.log(thomas);
 }
 
 main()
