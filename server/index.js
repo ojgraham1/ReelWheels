6a561f976a851eca1e95c29600a1ae65f1543fe5
Thomas <ThomasGrahamBiz@aol.com>
2024-06-27 22:22:25 -0400

    Trending movies displayed on browser.

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+const TMDB_API_KEY =
+  process.env.TMDB_API_KEY || "60bff7c4b3bc017974f0186538e281a6";
+const TMDB_BASE_URL = "https://api.themoviedb.org/3";
+
+app.use(cors());
+
+app.get("/api/movies", async (req, res) => {
+  try {
+    const response = await axios.get(`${TMDB_BASE_URL}/trending/movie/week`, {
+      params: {
+        api_key: TMDB_API_KEY,
+      },
+    });
+    res.json(response.data.results);
+  } catch (error) {
+    console.error("Error fetching movies from TMDB:", error);
+    res.status(500).json({ error: "Failed to fetch movies!" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
