c819f8f3e6654df93dac9aaf7cc8eac65c74938f
mimmo888 <mayaobeidat@gmail.com>
2024-06-28 14:11:21 -0400

    reservations db

--- server/api/db.js
+++ server/api/db.js
@@ -10,9 +10,7 @@ const getAllUsers = async () => {
 };
 
 const getUserById = async (id) => {
-    const response = await client.query(`SELECT * FROM Users WHERE id = $1`, [
-        id,
-    ]);
+    const response = await client.query(`SELECT * FROM Users WHERE id = $1`, [id]);
     return response.rows[0];
 };
 
@@ -27,12 +25,21 @@ const deleteUser = async (id) => {
 };
 
 const createUser = async (user) => {
-    const { username, password, firstName, lastName, email, address, phoneNumber, birthdate, isAdmin } = user;
-    const token = await jwt.sign({ id: user.id }, jwtSecret);
-    console.log("Token generated:", token);
+    // const { username, password, firstName, lastName, email, address, phoneNumber, birthdate, isAdmin } = user;
+    // const token = await jwt.sign({ id: user.id }, jwtSecret);
+    // // console.log("Token generated:", token);
+    // const response = await client.query(
+    // `INSERT INTO users (username, password, firstName, lastName, email, address, phoneNumber, birthdate, isAdmin) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,
+    // [username, await bcrypt.hash(password, 5), firstName, lastName, email, address, phoneNumber, birthdate, isAdmin]
+    // );
+    // return response.rows[0];
+};
+
+const updateUser = async (id, user) => {
+    const {username, password, firstName, lastName, email, address, phoneNumber, birthdate, isAdmin } = user;
     const response = await client.query(
-    `INSERT INTO users (username, password, firstName, lastName, email, address, phoneNumber, birthdate, isAdmin) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,
-    [username, password, firstName, lastName, email, address, phoneNumber, birthdate, isAdmin]
+      `UPDATE users SET username = $1, password = $2, firstName = $3, lastName = $4, email = $5, address = $6, phoneNumber = $7, birthdate = $8, isAdmin = $9 WHERE id = $10 RETURNING *`,
+      [username, password, firstName, lastName, email, address, phoneNumber, birthdate, isAdmin, id]
     );
     return response.rows[0];
 };
@@ -45,12 +52,53 @@ const createUser = async (user) => {
 // Showtimes
 
 // Reservations
+const getAllReservations = async () => {
+    const response = await client.query(`SELECT * FROM Reservations ORDER BY id ASC`);
+    return response.rows;
+};
+
+const getReservationById = async (id) => {
+    const response = await client.query(`SELECT * FROM Reservations WHERE id = $1`, [id]);
+    return response.rows[0];
+};
+
+const getReservationByUserId = async (params_id) => {
+    const response = await client.query(
+      `SELECT * FROM Reservations WHERE user_id = $1`,
+      [params_id]
+    );
+    return response.rows;
+  };
+  
+
+const deleteReservation = async (id) => {
+    await client.query(`DELETE FROM Reservations WHERE id = $1`, [id]);
+    return { id };
+};
+
+const createReservation = async (Reservations) => {
+    const { user_id, quantity, carpass, isPurchased, showtime_id, timePurchased } = Reservations;
+    const response = await client.query(
+        `INSERT INTO Reservations (user_id, quantity, carpass, isPurchased, showtime_id, timePurchased) VALUES ($1, $2, $3, $4, $5, $6 RETURNING *`,
+        [user_id, quantity, carpass, isPurchased, showtime_id, timePurchased]
+    );
+    return response.rows[0];
+};
 
 
 module.exports = {
+    // Users
     getAllUsers,
     getUserById,
     getUserByUsername,
     deleteUser,
     createUser,
+    updateUser,
+
+    // Reservations
+    getAllReservations,
+    getReservationById,
+    getReservationByUserId,
+    deleteReservation,
+    createReservation,
 }
