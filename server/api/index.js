5885c20c5ae640fad7c01d6590aafd9141b009e7
Curtis Patterson <Dpatter94@yahoo.com>
2024-07-01 16:43:40 -0400

    Added Showtimes route to Theater

--- server/api/index.js
+++ server/api/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/users", require("./users"));
-// router.use("/theater", require("./theater"))
-// router.use("/showtimes", require("./showtimes"))
+router.use("/theater", require("./theater"));
+router.use("/showtimes", require("./showtimes"));
 router.use("/reservations", require("./reservations"));
 
 module.exports = router;
