2bc7a0af38b8a4cabee86050e57496b97f879f88
mimmo888 <mayaobeidat@gmail.com>
2024-07-01 10:35:53 -0400

    push

--- server/api/index.js
+++ server/api/index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.use("/users", require("./users"))
-router.use("/theater", require("./theater"))
-router.use("/showtimes", require("./showtimes"))
-router.use("/reservations", require("./reservations"))
-router.use("/movieApi", require("./movieApi"))
+// router.use("/theater", require("./theater"))
+// router.use("/showtimes", require("./showtimes"))
+// router.use("/reservations", require("./reservations"))
 
 module.exports = router
--- server/api/movieApi.js
+++ server/api/movieApi.js
@@ -1,57 +1,6 @@
-// // const { PrismaClient } = require("@prisma/client");
-// // const axios = require("axios");
-// // const express = require("express");
-// // const pokemonRouter = express.Router();
 const express = require('express');
 const router = express.Router();
 
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient()
 
-// //paldea
-// pokemonRouter.get("/gen9", async (req, res, next) => {
-//   const response = await axios.get("https://pokeapi.co/api/v2/pokedex/31");
-
-//   res.send(response.data.pokemon_entries);
-// });
-
-// //kitakami
-// pokemonRouter.get("/gen9a", async (req, res, next) => {
-//   const response = await axios.get("https://pokeapi.co/api/v2/pokedex/32");
-
-//   res.send(response.data.pokemon_entries);
-// });
-
-// //blueberry academy
-// pokemonRouter.get("/gen9b", async (req, res, next) => {
-//   const response = await axios.get("https://pokeapi.co/api/v2/pokedex/33");
-
-//   res.send(response.data.pokemon_entries);
-// });
-
-// //   get pokemon by id
-// pokemonRouter.get("/:id", async (req, res, next) => {
-//   const response = await axios.get(
-//     `https://pokeapi.co/api/v2/pokemon/${req.params.id}`
-//   );
-
-//   res.send(response.data);
-// });
-// // get pokemon species by id
-// pokemonRouter.get("/species/:id", async (req, res, next) => {
-//   const response = await axios.get(
-//     `https://pokeapi.co/api/v2/pokemon-species/${req.params.id}`
-//   );
-
-//   res.send(response.data);
-// });
-// // get pokemon evolution chain
-// pokemonRouter.get("/evolution/:id", async (req, res, next) => {
-//   const response = await axios.get(
-//     `https://pokeapi.co/api/v2/evolution-chain/${req.params.id}`
-//   );
-
-//   res.send(response.data);
-// });
-
-// module.exports = pokemonRouter;
