6d5bae9b8b2b63fa005b775f6ad7f44df3d333f5
Thomas <ThomasGrahamBiz@aol.com>
2024-07-01 11:06:57 -0400

    deleted files

--- server/api/users.js
+++ server/api/users.js
@@ -1,22 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient()
-
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 // Get All Users
-router.get('/', async (req, res) => {
-    try {
-        const users = await prisma.users.findMany ({
-            select: {
-                username: true
-            }
-        });
-        res.status(200).send(users)
-    } catch (error) {
-        res.status(500).send({ msg: error.message })
-    };
+router.get("/", async (req, res) => {
+  try {
+    const users = await prisma.users.findMany({
+      select: {
+        username: true,
+      },
+    });
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(500).send({ msg: error.message });
+  }
 });
 
 // export const getAllUsers = async (req, res) => {
@@ -30,58 +29,68 @@ router.get('/', async (req, res) => {
 
 // Get User By Id
 router.get("/:id", async (req, res) => {
-    try {
-        const user = await prisma.users.findUnique({
-            where: {
-                id: Number(req.params.id),
-            },
-            select: {
-                username: true
-            }
-        });
-        res.status(200).json(user)
-    } catch (error) {
-        res.status(404).json({ msg: error.message })
-    };
+  try {
+    const user = await prisma.users.findUnique({
+      where: {
+        id: Number(req.params.id),
+      },
+      select: {
+        username: true,
+      },
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ msg: error.message });
+  }
 });
 
 // Get User By Username
 // ? Check if username: username() is correct input for this field
 router.get("/:username", async (req, res) => {
-    try {
-        const user = await prisma.users.findUnique({
-            where: {
-                username: (req.body.username),
-            },
-        })
-        res.status(200).json(user)
-    } catch (error) {
-        res.status(404).json({ msg: error.message })
-    };
+  try {
+    const user = await prisma.users.findUnique({
+      where: {
+        username: req.body.username,
+      },
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ msg: error.message });
+  }
 });
 
 // Create New User
 // ? check if boolean is correct input
 router.post("/", async (req, res) => {
-    const { username, password, firstName, lastName, email, address, phoneNumber, birthdate, isAdmin  } = req.body
-    try {
-        const newUser = await prisma.users.create({
-            data: {
-                username: username, 
-                password: password, 
-                firstName: firstName, 
-                lastName: lastName, 
-                email: email, 
-                address: address, 
-                phoneNumber: phoneNumber, 
-                birthdate: birthdate, 
-                isAdmin: Boolean, 
-            },
-        })
-        res.status(201).json(newUser)
-    } catch (error) {
-        res.status(400).json({ msg: error.message })
-    };
+  const {
+    username,
+    password,
+    firstName,
+    lastName,
+    email,
+    address,
+    phoneNumber,
+    birthdate,
+    isAdmin,
+  } = req.body;
+  try {
+    const newUser = await prisma.users.create({
+      data: {
+        username: username,
+        password: password,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        address: address,
+        phoneNumber: phoneNumber,
+        birthdate: birthdate,
+        isAdmin: Boolean,
+      },
+    });
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
 });
 
 // export const createUser = async (req, res) => {
@@ -89,15 +98,15 @@ router.post("/", async (req, res) => {
 //     try {
 //         const user = await prisma.users.create({
 //             data: {
-//                 username: username, 
-//                 password: password, 
-//                 firstName: firstName, 
-//                 lastName: lastName, 
-//                 email: email, 
-//                 address: address, 
-//                 phoneNumber: phoneNumber, 
-//                 birthdate: birthdate, 
-//                 isAdmin: Boolean, 
+//                 username: username,
+//                 password: password,
+//                 firstName: firstName,
+//                 lastName: lastName,
+//                 email: email,
+//                 address: address,
+//                 phoneNumber: phoneNumber,
+//                 birthdate: birthdate,
+//                 isAdmin: Boolean,
 //             },
 //         })
 //         res.status(201).json(user)
@@ -108,42 +117,52 @@ router.post("/", async (req, res) => {
 
 // // Update Existing User
 router.put("/:id", async (req, res) => {
-    const { username, password, firstName, lastName, email, address, phoneNumber, birthdate, isAdmin } = req.body
-    try {
-        const user = await prisma.users.update({
-            where: {
-                id: Number(req.params.id),
-            },
-            data: {
-                username: username, 
-                password: password, 
-                firstName: firstName, 
-                lastName: lastName, 
-                email: email, 
-                address: address, 
-                phoneNumber: phoneNumber, 
-                birthdate: birthdate, 
-                isAdmin: Boolean, 
-            },
-        })
-        res.status(200).json(user)
-    } catch (error) {
-        res.status(400).json({ msg: error.message })
-    };
+  const {
+    username,
+    password,
+    firstName,
+    lastName,
+    email,
+    address,
+    phoneNumber,
+    birthdate,
+    isAdmin,
+  } = req.body;
+  try {
+    const user = await prisma.users.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: {
+        username: username,
+        password: password,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        address: address,
+        phoneNumber: phoneNumber,
+        birthdate: birthdate,
+        isAdmin: Boolean,
+      },
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
 });
 
 // // Delete Existing User
 router.delete("/:id", async (req, res) => {
-    try {
-        const user = await prisma.users.delete({
-            where: {
-                id: Number(req.params.id),
-            },
-        })
-        res.status(200).json(user)
-    } catch (error) {
-        res.status(400).json({ msg: error.message })
-    };
+  try {
+    const user = await prisma.users.delete({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
 });
 
 module.exports = router;
