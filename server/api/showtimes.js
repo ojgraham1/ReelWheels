44f0eb85030758ecb8e5c683299767a3913bf7b8
Thomas <ThomasGrahamBiz@aol.com>
2024-07-02 17:44:39 -0400

    Theaters and Showtimes sync together. - Showtimes are automatically being seeded into theaters. - Showtimes are capped at 5 shows for each theater. - New theaters have been created with real locations and unique emails. - This code is subject to change, but this is a good point to be at..

--- server/api/showtimes.js
+++ server/api/showtimes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
@@ -13,96 +12,5 @@ router.get("/", async (req, res) => {
     res.status(500).json({ msg: error.message });
   }
 });
-
-//get Showtimes by Theater ID
-// router.get("Theater/Showtimes/:id", async (req, res) => {
-//   try {
-//     const ShowtimesByTheaterId = await prisma.showtimes.findUnique({
-//       where: {
-//         theater_id: Number(req.params.id),
-//       },
-//     });
-//     res.status(200).json(ShowtimesByTheaterId);
-//   } catch (error) {
-//     res.status(404).json({ msg: error.message });
-//   }
-// });
-
-// export const getShowtimesByTheaterId = async (req, res) => {
-//   try {
-//     const response = await prisma.showtimes.findUnique({
-//       where: {
-//         theater_id: Number(req.params.id),
-//       },
-//     });
-//     res.status(200).json(response);
-//   } catch (error) {
-//     res.status(404).json({ msg: error.message });
-//   }
-// };
-
-// export const getShowtimesByMovieId = async (req, res) => {
-//   try {
-//     const response = await prisma.showtimes.findUnique({
-//       where: {
-//         movie_id: Number(req.params.id),
-//       },
-//     });
-//     res.status(200).json(response);
-//   } catch (error) {
-//     res.status(404).json({ msg: error.message });
-//   }
-// };
-
-// // CONTINUE WORKING HERE
-// export const createSho = async (req, res) => {
-//   const { Location, Address, Capacity, email } = req.body;
-//   try {
-//     const theater = await prisma.theater.create({
-//       data: {
-//         Location: Location,
-//         Address: Address,
-//         Capacity: Capacity,
-//         email: email,
-//       },
-//     });
-//     res.status(201).json(theater);
-//   } catch (error) {
-//     res.status(400).json({ msg: error.message });
-//   }
-// };
-
-// export const updateTheater = async (req, res) => {
-//   const { Location, Address, Capacity, email } = req.body;
-//   try {
-//     const theater = await prisma.theater.update({
-//       where: {
-//         id: Number(req.params.id),
-//       },
-//       data: {
-//         Location: Location,
-//         Address: Address,
-//         Capacity: Capacity,
-//         email: email,
-//       },
-//     });
-//     res.status(200).json(theater);
-//   } catch (error) {
-//     res.status(400).json({ msg: error.message });
-//   }
-// };
-
-// export const deleteTheater = async (req, res) => {
-//   try {
-//     const theater = await prisma.theater.delete({
-//       where: {
-//         id: Number(req.params.id),
-//       },
-//     });
-//     res.status(200).json(theater);
-//   } catch (error) {
-//     res.status(400).json({ msg: error.message });
-//   }
-// };
 
 module.exports = router;
