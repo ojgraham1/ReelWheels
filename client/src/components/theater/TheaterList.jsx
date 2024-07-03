7fc49a5d8a152d88e56f2d4b38844e77868ebea8
Curtis Patterson <Dpatter94@yahoo.com>
2024-07-03 15:26:55 -0400

    Added Theater List for all the theaters

--- client/src/components/theater/TheaterList.jsx
+++ client/src/components/theater/TheaterList.jsx
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function MoviePage() {
-    return (
-        <div className="ex">Hello Theaters!</div>
-    );
-};
+  const [Theater, setTheaters] = useState([]);
+  useEffect(() => {
+    const fetchTheaters = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/theater");
+        setTheaters(response.data);
+      } catch (error) {
+        console.error("Error fetching theaters:", error);
+      }
+    };
+
+    fetchTheaters();
+  });
+  return (
+    <div className="tContainer">
+      <ul className="theater-container">
+        <h1 className="tHeading">THEATERS</h1>
+        <div className="tWrapper">
+          <div className="tCard-Container">
+            {Theater.map((theater) => (
+              <div className="tCard">
+                <ul className="tCardWrapper" key={theater.id}>
+                  <div className="tText-Container">
+                    <h2 className="tLocation">{theater.Location}</h2>
+                    <p className="tAddress">{theater.Address}</p>
+                    <p className="tCapacity">{theater.Capacity}</p>
+                    <p className="tEmail">{theater.email}</p>
+
+                    <button className="tMoreInfoBut">More Info</button>
+                  </div>
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
+      </ul>
+    </div>
+  );
+}
