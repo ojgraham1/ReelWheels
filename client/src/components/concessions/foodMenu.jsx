19c9a8852765c426a9b1b5b5155ff8180ff64169
C Daniel Patterson <dpatter94@yahoo.com>
2024-07-11 22:44:21 -0400

    Renamed Menu file, added button to go to/open menu pdf

--- client/src/components/concessions/foodMenu.jsx
+++ client/src/components/concessions/foodMenu.jsx
@@ -1,5 +1,16 @@
 export default function Concessions() {
-    return (
-        <div className="ex">Hello Food!</div>
-    );
-};
+  return (
+    <div>
+      <div className="ex">Hello Food!</div>
+      <button className="MenuBut">
+        <a
+          href="../public/ReelWheelsMenu.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click Here For Menu
+        </a>
+      </button>
+    </div>
+  );
+}
