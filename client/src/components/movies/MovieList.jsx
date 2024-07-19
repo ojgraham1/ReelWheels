216a564fb614b2e290eddfb3981eab06e36d25aa
mimmo888 <mayaobeidat@gmail.com>
2024-07-19 13:21:51 -0400

    collective push for multiple styling changes

--- client/src/components/movies/MovieList.jsx
+++ client/src/components/movies/MovieList.jsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTicket, faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 import ShowtimesModal from "./ShowtimesModal";
-import DateTime from "../directory/DateTime";
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
@@ -55,9 +54,6 @@ const MovieList = () => {
       <ul className="movie-list-container">
         <h1 className="mLHeading">IN THEATERS NOW</h1>
         <div className="mLWrapper">
-          {/* <div className="mlDateTime">
-            <DateTime />
-          </div> */}
           <div className="mlCard-Container">
             {movies.map((movie) => (
               <div className="mlCard" key={movie.id}>
--- client/src/components/movies/MoviePage.jsx
+++ client/src/components/movies/MoviePage.jsx
@@ -78,9 +78,6 @@ function MoviePage() {
                     </div>
                 )}
             </div>
-            <div className="mpShowtimes-Container">
-                <ShowtimesMovie />
-            </div>
         </div>
     );
 }
