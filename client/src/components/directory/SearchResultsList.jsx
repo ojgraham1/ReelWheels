361a64354583b751c1679151df84e9f47c339c1d
Curtis Patterson <Dpatter94@yahoo.com>
2024-07-19 23:39:29 -0400

    Commit

--- client/src/components/directory/SearchResultsList.jsx
+++ client/src/components/directory/SearchResultsList.jsx
@@ -16,26 +16,30 @@ import { SearchResult } from "./SearchResult";
 export const SearchResultsList = ({ browseResults, tvResults }) => {
   return (
     <div>
-      {browseResults && browseResults.length > 0 && (
-        <div className="results-list">
-          {browseResults.map((browseResult, id) => {
-            return <SearchResult result={browseResult.title} key={id} />;
-          })}
-        </div>
-      )}
-      {tvResults && tvResults.length > 0 && (
-        <div className="results-list">
-          {tvResults.map((tvResult, id) => {
-            return <SearchResult result={tvResult.original_name} key={id} />;
-          })}
-        </div>
-      )}
-      ;
+      <div className="Results-Lists">
+        {browseResults && browseResults.length > 0 && (
+          <div className="browse-Result-List">
+            {browseResults.map((browseResult, id) => {
+              return <SearchResult result={browseResult.title} key={id} />;
+            })}
+          </div>
+        )}{" "}
+        {tvResults && tvResults.length > 0 && (
+          <div className="tv-Result-List">
+            {tvResults.map((tvResult, id) => {
+              return <SearchResult result={tvResult.original_name} key={id} />;
+            })}
+          </div>
+        )}
+        ;
+      </div>
     </div>
   );
 };
 
-// SearchResultsList.propTypes = {
-//   browseResult: PropTypes.arrayOf(PropTypes.object).isRequired,
-//   tvResult: PropTypes.arrayOf(PropTypes.object).isRequired,
-// };
+SearchResultsList.propTypes = {
+  browseResults: PropTypes.arrayOf(PropTypes.object).isRequired,
+  browseResult: PropTypes.arrayOf(PropTypes.object).isRequired,
+  tvResults: PropTypes.arrayOf(PropTypes.object).isRequired,
+  tvResult: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
