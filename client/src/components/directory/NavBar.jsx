0a6139804099b49b9fcb40651f91c196b8625fb8
Thomas <ThomasGrahamBiz@aol.com>
2024-07-22 20:31:00 -0400

    New sign out icon uploaded!!

--- client/src/components/directory/NavBar.jsx
+++ client/src/components/directory/NavBar.jsx
@@ -3,7 +3,8 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { clearToken } from "../../api/sliceAuth";
 import DateTime from "./DateTime";
-import "../../styles/directory/nav.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
 export default function NavBar() {
   const token = useSelector((state) => state.auth.token);
@@ -55,8 +56,8 @@ export default function NavBar() {
               </NavLink>
             </li>
             <li>
-              <button className="nL-logout" onClick={handleLogout}>
-                Logout
+              <button className="nL-logout-icon" onClick={handleLogout}>
+                <FontAwesomeIcon icon={faSignOutAlt} />
               </button>
             </li>
           </>
